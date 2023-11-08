ec5760b1316981421a40345034ae0b43df4838b2
JurassicAlbert <albertjurasik@gmail.com>
2023-11-08 15:10:45 +0100

    configure GET endpoint

--- currencies_exchange/views/currency_api_views.py
+++ currencies_exchange/views/currency_api_views.py
@@ -1,6 +1,16 @@
-from rest_framework.views import APIView
+from rest_framework.response import Response
+from currencies_exchange.models import Currency
+from rest_framework.views import APIView, status
+from currencies_exchange.serializers import CurrencySerializer
 
 
 class CurrencyAPIView(APIView):
     """Currency API for fetch a list of all currencies"""
-    pass
+
+    def get(self, request):
+        all_currencies = Currency.objects.all()
+        if not all_currencies:
+            return Response("Unable to find currencies data", status=status.HTTP_404_NOT_FOUND)
+        else:
+            serialized_currencies = CurrencySerializer(all_currencies, many=True)
+            return Response(serialized_currencies.data, status=status.HTTP_200_OK)
