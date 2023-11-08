1c9d0390ba1d9dd7de09ece9f6b2368e24eb9975
JurassicAlbert <albertjurasik@gmail.com>
2023-11-08 13:42:33 +0100

    prepare currency api class

---
+++ currencies_exchange/views/__init__.py
@@ -0,0 +1,1 @@
+from .currency_api_views import CurrencyAPIView
---
+++ currencies_exchange/views/currency_api_views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+
+
+class CurrencyAPIView(APIView):
+    """Currency API for fetch a list of all currencies"""
+    pass
