11d93514e3cbfcb0f0c0443b51d472e040ed6803
JurassicAlbert <albertjurasik@gmail.com>
2023-11-09 12:43:59 +0100

    add sorting and search

--- currencies_exchange/filters/currency_filter.py
+++ currencies_exchange/filters/currency_filter.py
@@ -8,6 +8,8 @@ class CurrencyFilter(django_filters.FilterSet):
     currency_symbol = django_filters.CharFilter(lookup_expr='icontains')
     currency_code = django_filters.CharFilter(lookup_expr='icontains')
 
+
     class Meta:
         model = Currency
         fields = ['currency_name', 'currency_symbol', 'currency_code']
+        # ordering = ['currency_name', 'currency_symbol', 'currency_code']
--- currencies_exchange/views/currency_api_views.py
+++ currencies_exchange/views/currency_api_views.py
@@ -1,25 +1,21 @@
-import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework import generics
 from currencies_exchange.models import Currency
 from currencies_exchange.filters import CurrencyFilter
 from currencies_exchange.serializers import CurrencySerializer
 
 
-# class CurrencyAPIView(APIView):
-#     """Currency API for fetch a list of all currencies"""
-#
-#     def get(self, request):
-#         all_currencies = Currency.objects.all()
-#         serialized_currencies = CurrencySerializer(all_currencies, many=True)
-#         if not serialized_currencies:
-#             return Response("Unable to find currencies data", status=status.HTTP_404_NOT_FOUND)
-#         else:
-#             return Response(serialized_currencies.data, status=status.HTTP_200_OK)
-
-
 class CurrencyAPIView(generics.ListAPIView):
     """Currency API for fetch a list of all currencies"""
-    queryset = Currency.objects.all()
+    # queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_class = CurrencyFilter
+    search_fields = ['currency_name', 'currency_symbol', 'currency_code']
+    ordering_fields = '__all__'
+    ordering = ['currency_code']
+
+    def get_queryset(self):
+        queryset = Currency.objects.all()
+        return queryset
